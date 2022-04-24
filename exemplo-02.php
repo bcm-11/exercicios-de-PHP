a6b7f947e9cf697163ed4e46e45835cec6bb5b3f
Bruno Caminha Morais <bruno.caminha11@gmail.com>
2022-04-24 16:28:20 -0300

    commit exemplos 01 e 02

---
+++ exemplo-02.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+$anoNascimento = 1986;
+*/
+
+//uso de barra+asterisco faz o php ignorar o trecho em questão
+
+$nomeCompleto = "José da Silva";
+
+$nome1 = "José";
+
+echo $nomeCompleto;
+
+echo "<br/>";
+//para pular linha
+
+if (isset($nome1)) {
+
+	echo $nome1;
+}
+//se a variávem $nome1 existir, mostrar
+
+?>
